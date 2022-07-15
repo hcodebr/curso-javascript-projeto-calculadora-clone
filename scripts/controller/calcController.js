b0729c874f105119184371d1c74ec1081de18ed6
Danilo <dap4perez@gmail.com>
2022-07-15 17:18:29 -0300

    aula 13

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -8,6 +8,7 @@ class CalcController {
         this._timeEl = document.querySelector("#hora");
         this._currentDate;
         this.initialize();
+        this.initButtonsEvents();
 
     }
 
@@ -23,9 +24,35 @@ class CalcController {
 
     }
 
+    addEventListenerAll(element, events, fn){
+
+        events.split(' ').forEach(event => {
+
+            element.addEventListener(event, fn, false);
+
+        })
+    
+    }
+
     initButtonsEvents(){
 
         let buttons = document.querySelectorAll("#buttons > g, #parts > g");
+
+        buttons.forEach((btn, index)=>{
+
+            this.addEventListenerAll(btn, "click drag", e => {
+
+                console.log(btn.className.baseVal.replace("btn-",""));
+
+            })
+
+            this.addEventListenerAll(btn, "mouseover mouseup mousedown", e => {
+
+                btn.style.cursor = "pointer";
+
+            })
+
+        })
 
     }
 
