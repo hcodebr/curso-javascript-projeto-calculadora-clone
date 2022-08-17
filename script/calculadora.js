8859d0240abe4fa1353f68aecb02bef548b95af1
Adriano gomess <adrianoecirlerne@gmail.com>
2022-08-17 05:33:49 -0300

    commit

--- script/calculadora.js
+++
@@ -1,1 +0,0 @@
-let calculadora = new CalcController();
--- script/controller/CalcController.js
+++ script/controller/CalcController.js
@@ -1,8 +1,88 @@
 class CalcController{
 
     constructor(){
-        this.displayCalc = "0";
-        this.dataAtual = "";
+        this._locale = 'pt-br';
+        this._displayCalcEl = document.querySelector("#display");
+        this._dateEL= document.querySelector("#data");
+        this._timeEl = document.querySelector("#hora");
+        this._currentDate = "";
+        this.initialize();
+        this.initButtonsEvents();
 
     }
+
+    initialize(){
+
+        this.setDisplayDateTime();
+
+        setInterval(() =>{
+            this.setDisplayDateTime();
+
+           
+        }, 1000);
+   
+    }
+
+    initButtonsEvents(){
+
+       let buttons = document.querySelectorAll("#buttons > g, #parts > g");    //seleciona os botoes da calc pela classe 
+
+        buttons.forEach((btn, index)=>{
+
+            btn.addEventListener('click', e => {
+
+                console.log(btn.className.baseVal.replace("btn-",""));
+
+
+            });
+       })
+    }
+
+    setDisplayDateTime(){
+
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale);  //atualiza a data em tempo real 
+        this.displayTime = this.currentDate.toLocaleTimeString(this._locale);  //atualiza a hora em tempo real 
+
+    }
+    get displayTime(){
+        return this._timeEl.innerHTML;
+
+    }
+    set displayTime(value){
+        return this._timeEl.innerHTML = value;
+
+    }
+
+    get displayDate(){
+        return this._dateEL.innerHTML;
+
+    }
+    set displayDate(value){
+        return this._dateEL.innerHTML = value;
+
+    }
+
+    get displayCalc(){
+        return this._displayCalcEl.innerHTML;
+    }
+
+    set displayCalc(value){
+        this._displayCalcEl.innerHTML = value;
+    }
+
+    get currentDate(){
+        return new Date(); 
+    }
+
+    set currentDate(value){
+        return this._dataAtual = value;
+    }
+
+
+
+
+
+
+
+
 }
