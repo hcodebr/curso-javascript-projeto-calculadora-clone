933102f9876ad2f4dd1a9407a80de431c12a5116
marcelo <joao69@aluno.senai-ce.org.br>
2023-05-12 09:36:48 -0300

    adicionando metodo initialize

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -2,7 +2,12 @@ class CalcController {
 
     constructor(){
         this._displayCalc = "0";
-        this.currentDate;
+        this._currentDate;
+        this.initialize();
+    }
+
+    initialize(){
+
     }
     
     get displayCalc(){
