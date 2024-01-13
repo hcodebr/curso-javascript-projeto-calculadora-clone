48388f8bca760789bfe5b92c4a539434361d1ed3
LUCAS GABRIEL DA SILVA VAZ SEVERO <155263464+Lucassevero06@users.noreply.github.com>
2024-01-13 16:04:42 -0400

    mouseup,  mouseover, mousedown, cursor pointer

--- scripts/controller/CalController.js
+++ scripts/controller/CalController.js
@@ -24,19 +24,35 @@ class CalController {
 
     }
 
+    addEventListenerAll(element, events, fn){
+
+        events.split(' ').forEach(event => {
+
+            element.addEventListener(event, fn, false);
+
+        });
+
+    }
+
     initButtonsEvents(){
 
         let buttons = document.querySelectorAll("#buttons > g, #parts > g");
 
         buttons.forEach((btn, index)=>{
 
-            btn.addEventListener('click', e => {
+            this.addEventListenerAll(btn, "click drag", e => {
 
                 console.log(btn.className.baseVal.replace("btn-",""));
 
             });
 
-        })
+            this.addEventListenerAll(btn, "mouseover mouseup mousedown", e => {
+
+                btn.style.cursor = "pointer";
+
+            });
+
+        });
 
     }
 
