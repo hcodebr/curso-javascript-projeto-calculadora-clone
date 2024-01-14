1715f4c1c7cb045dccb526a12a5e468f38b2492d
LUCAS GABRIEL DA SILVA VAZ SEVERO <155263464+Lucassevero06@users.noreply.github.com>
2024-01-13 20:15:44 -0400

    addOperation, clearentry, clearall, seterror, execbtn

--- scripts/controller/CalController.js
+++ scripts/controller/CalController.js
@@ -2,6 +2,7 @@ class CalController {
 
     constructor(){
 
+        this._operation = [];
         this._locale = 'pt-BR';
         this._displayCalcEl = document.querySelector("#display");
         this._dateEl = document.querySelector("#date");
@@ -24,13 +25,95 @@ class CalController {
 
     }
 
-    addEventListenerAll(element, events, fn){
+    addEventListenerAll(element, event, fn){
 
-        events.split(' ').forEach(event => {
+        event.split(' ').forEach(event => {
 
             element.addEventListener(event, fn, false);
 
         });
+
+    }
+
+    clearAll(){
+
+        this._operation = [];
+
+    }
+
+    clearEntry(){
+
+        this._operation.pop();
+
+    }
+
+    addOperation(value){
+
+        this._operation.push(value);
+
+        console.log(this._operation);
+
+    }
+
+    setError(){
+
+        this.displayCalc = "Error";
+
+    }
+
+    execBtn(value){
+
+        switch (value) {
+
+            case 'ac':
+                this.clearAll();
+            break;
+
+            case 'ce':
+                this.clearEntry();
+            break;
+
+            case 'soma':
+
+            break;
+
+            case 'subtracao':
+
+            break;
+
+            case 'divisao':
+
+            break;
+
+            case 'multiplicacao':
+
+            break;
+
+            case 'porcento':
+
+            break;
+
+            case 'igual':
+
+            break;
+
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+            this.addOperation(parseInt(value));
+                break;
+
+            default:
+                this.setError();
+                break;
+        }
 
     }
 
@@ -42,7 +125,9 @@ class CalController {
 
             this.addEventListenerAll(btn, "click drag", e => {
 
-                console.log(btn.className.baseVal.replace("btn-",""));
+                let textBtn = btn.className.baseVal.replace("btn-", "");
+
+                this.execBtn(textBtn);
 
             });
 
@@ -84,7 +169,7 @@ class CalController {
     }
 
     set displayCalc(value){
-        this._displayCalc = value;
+        this._displayCalcEl.innerHTML = value;
     }
 
     get currentDate(){
